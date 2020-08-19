ec09d875c523df1cb3ed777c23a74ed1f93d1e27
Serkan Sahin <ssahin55@outlook.com>
2020-08-19 15:05:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count = 0
   new_array = []
   while count < array.length do
-    new_array.push(array[count] ** array[count])
+    new_array.push(array[count] ** 2 )
     count += 1
   end
   puts new_array

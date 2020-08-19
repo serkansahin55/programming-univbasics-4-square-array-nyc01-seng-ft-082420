cb44ae19bd8bfaa9962381cae748f063ac3681b8
Serkan Sahin <ssahin55@outlook.com>
2020-08-19 15:04:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count = 0
   new_array = []
   while count < array.length do
-    new_array.push(array[count] ** 2)
+    new_array.push(array[count] ** srray[count])
   end
   puts new_array
 end
